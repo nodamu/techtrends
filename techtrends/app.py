e06b9fbe5a5f078f1496d5839476a930af68d1ec
Mike Perry Y Attara <mpyebattara@gmail.com>
2021-02-04 13:23:38 +0000

    comment global variable

--- techtrends/app.py
+++ techtrends/app.py
@@ -3,6 +3,7 @@ import sqlite3
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+# Count all database connections
 connection_count = 0
 
 
